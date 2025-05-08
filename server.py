cb10f62b0e7d4392629e0a31301127cda6b6fd06
Raquel <leuqar999@gmail.com>
2025-05-08 11:13:46 +0200

    segundo cambio

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
-from flask import Flask
+pfrom flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "caquelita on fire"
+    return "Empieza lo bueno!"
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
-            
